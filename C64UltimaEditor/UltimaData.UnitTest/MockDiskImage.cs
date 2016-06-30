8f16bf7cf8d3435a707b532872da21f36fa07682
Chris Mallery <chmaller@microsoft.com>
2016-06-30 14:59:31 -0700

    Working on a mock

---
+++ C64UltimaEditor/UltimaData.UnitTest/MockDiskImage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimaData.UnitTest
+{
+    public class MockImageFile : IImageFile
+    {
+        MockImageFile(byte[] data)
+        {
+            Data = data;
+        }
+
+        public void Close()
+        {
+          
+        }
+
+        public int Read(byte[] buffer, int len)
+        {
+            if (len < Data.Length)
+            {
+                Array.Copy(Data, buffer, len);
+                return len;
+            }
+            else
+            {
+                Array.Copy(Data, buffer, Data.Length);
+                return Data.Length;
+            }
+        }
+
+        public int Write(byte[] buffer, int len)
+        {
+            Data = new byte[len];
+            Array.Copy(buffer, Data, len);
+            return len;
+        }
+
+        public byte[] Data;
+    }
+
+    public class MockDiskImage
+    {
+        MockDiskImage()
+        {
+            Files = new Dictionary<string, MockImageFile>();
+        }
+
+        public Dictionary<string, MockImageFile> Files;
+    }
+}
